f648fbe626beedbc8287a6a1e51c4aa4a93ae247
Álisson de Moraes Zimermann <alissonzima@gmail.com>
2025-01-14 22:54:32 -0300

    Create chrome.bat

---
+++ chrome.bat
@@ -0,0 +1,1 @@
+"c:\temp\GoogleChromePortable\GoogleChromePortable.exe" -remote-debugging-port=9014 --user-data-dir="c:\temp\GoogleChromePortable\Chrome_Test" --window-position=0,0 --window-size=1024,768
